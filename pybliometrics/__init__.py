d6c3209d34653da275c02ddd6b8582674652819c
Razvan-Nicolae Nancu <e1502339@student.tuwien.ac.at>
2025-03-18 08:39:45 +0100

    initial commit

---
+++ pybliometrics/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version
+
+__version__ = version("pybliometrics")
+
+__citation__ = 'Rose, Michael E. and John R. Kitchin: "pybliometrics: '\
+    'Scriptable bibliometrics using a Python interface to Scopus", SoftwareX '\
+    '10 (2019) 100263.'
+
+import pybliometrics.scopus
+import pybliometrics.sciencedirect
